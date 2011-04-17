809855be82b4028e8ae2139cded8876259e8ef03
tv <tv@also>
2011-04-17 06:23:34 +0200

    index.js: only collect and forward data to client[s]

--- index.js
+++ index.js
@@ -18,39 +18,35 @@ var io = require('socket.io');
 var socket = io.listen(server);
 var client_send = function () {};
 socket.on('connection', function (client) {
+  var last = new Date();
+  var buf = {};
   client_send = function (x) {
-    client.send(x);
+    var key = JSON.stringify(x);
+    if (key in buf) {
+      buf[key]++;
+    } else {
+      buf[key] = 1;
+    };
+    var now = new Date();
+    if ((now - last) > 1000/30) {
+      Object.keys(buf).forEach(function (key) {
+        var m = key.replace(']', ',' + buf[key] + ']');
+        client.send(m);
+      });
+      buf = {};
+      last = now;
+    };
   };
 });
-
 
 tcpdump = spawn('sudo', [ 'tcpdump-as-nobody' ]);
 
 tcpdump.stdout.on('data', function (data) {
-  var r = 25;
-  var a = r, b = r;
   data.toString().split(/\n/g).forEach(function (line) {
     var info = /^IP ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\.([0-9]+) > ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\.([0-9]+).*/.exec(line);
     if (info) {
-      var src = IPv4.to_coords(info[1], r);
-      var dst = IPv4.to_coords(info[3], r);
-
-      if (JSON.stringify(src) != JSON.stringify(dst)) {
-        log(+new Date, src, dst);
-        client_send(JSON.stringify([info[1],info[3]]));
-      };
+      client_send([info[1],info[3]]);
     };
   });
-  //console.log('data', //.exec(data));
 });
 
-
-//function print_javascript() {
-//  console.log(''
-//    + IPv4.to_coords.toString() + ';'
-//    + IPv4.to_int.toString()
-//    + IPv4.to_int.toString()
-//  );
-//};
-//
-//print_javascript();
